4bfb385739879be543858d31006ff63da8aa38e2
Nicole <nicolecarpenterusa@gmail.com>
2016-06-08 14:06:37 -0500

    create config file to handle constants

--- lib/computer_move_generator.py
+++ lib/computer_move_generator.py
@@ -1,36 +1,35 @@
 import abc
 import copy
+import config
 from lib.move_generator import MoveGenerator
 
 class ComputerMoveGenerator(MoveGenerator):
 
   def __init__(self, view):
     self.view = view
+    self.MAX_DEPTH = 7
+    self.STARTING_DEPTH = 0
     
-  def select_space(self, board, player_marker):
-    current_player = player_marker
-    opponent = self.__find_opponent_marker(player_marker)
-    starting_depth = 0
+  def select_space(self, board, current_player_marker):
+    opponent_marker = self.__find_opponent_marker(current_player_marker)
     self.view.display_computer_thinking()
-    return self.__negamax(board, current_player, opponent, starting_depth, {})
+    return self.__negamax(board, current_player_marker, opponent_marker, self.STARTING_DEPTH, {})
 
-  def __negamax(self, board, player, opponent, depth, negamax_scores):
-    max_depth = 7
-    if depth >= max_depth:
+  def __negamax(self, board, player_marker, opponent_marker, depth, negamax_scores):
+    if depth >= self.MAX_DEPTH:
       return 0
     if board.is_tie_condition_met() or board.find_winning_marker() != None:
       return self.__score(board)
         
     for space in board.find_open_spaces():
       temp_board = copy.deepcopy(board)
-      temp_board.place_piece(player, space + 1)
-      negamax_scores[space] = -1 * self.__negamax(temp_board, opponent, player, depth + 1, {})
+      temp_board.place_piece(player_marker, space + 1)
+      negamax_scores[space] = -1 * self.__negamax(temp_board, opponent_marker, player_marker, depth + 1, {})
 
-    starting_depth = 0
     best_spot_index = max(negamax_scores, key=negamax_scores.get)
     highest_negamax_value = negamax_scores[best_spot_index]
 
-    if depth != starting_depth:
+    if depth != self.STARTING_DEPTH:
       return highest_negamax_value
     else:
       return best_spot_index + 1
@@ -41,8 +40,8 @@ class ComputerMoveGenerator(MoveGenerator):
     else:
       return -10
 
-  def __find_opponent_marker(self, player):
-    if player == 'X':
-      return 'O'
+  def __find_opponent_marker(self, current_player_marker):
+    if current_player_marker == config.PLAYER_MARKER:
+      return config.OPPONENT_MARKER
     else:
-      return 'X'
+      return config.PLAYER_MARKER
