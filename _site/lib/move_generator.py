6ff6267e96b4edf6800864fc5cb334e834d54f9a
Nicole <nicolecarpenterusa@gmail.com>
2016-06-08 10:45:31 -0500

    remove _site files

--- _site/lib/move_generator.py
+++
@@ -1,8 +0,0 @@
-import abc
-
-class MoveGenerator(object):
-  __metaclass__ = abc.ABCMeta
-
-  @abc.abstractmethod
-  def select_space(self, board, player_marker):
-    pass
--- _site/lib/player.py
+++
@@ -1,6 +0,0 @@
-class Player(object):
-
-  def __init__(self, name, marker, move_generator):
-    self.name = name
-    self.marker = marker
-    self.move_generator = move_generator
